89851a2d167d9a4b3c1787ae1bbfddeb91cff2f9
Julien Vong <jvong@uwaterloo.ca>
2019-05-25 02:32:50 -0400

    WIP

--- mta_whr.js
+++ mta_whr.js
@@ -1,6 +1,50 @@
 let players = []
 let ratings = []
 let sets = []
+let filterDays = -1;
+
+function displayPlayerRanking() {
+    let mostRecentPlayerRating = {}
+
+    // There's very likely a better way to do this.
+    ratings.forEach(function(rating) {
+        if (!(rating["id"] in mostRecentPlayerRating)) {
+            mostRecentPlayerRating["id"] = {
+                "day": rating["day"],
+                "rating": rating["rating"],
+            }
+        }
+
+        if (mostRecentPlayerRating["id"]["day"] < rating["day"]) {
+            mostRecentPlayerRating["id"] = {
+                "day": rating["day"],
+                "rating": rating["rating"],
+            }
+        }
+    });
+
+    let ranking = Object.keys(mostRecentPlayerRating).map(function(key) {
+        return {
+            "id": key,
+            "day": mostRecentPlayerRating[key]["day"],
+            "rating": mostRecentPlayerRating[key]["rating"],
+        }
+    });
+
+    ranking.sort(function(first, second) {
+        return second["rating"] - first["rating"];
+    });
+
+    console.log(ranking);
+}
+
+function update() {
+    if (players.length == 0 || ratings.length == 0 || sets.length == 0) {
+        return;
+    }
+
+    displayPlayerRanking();
+}
 
 fetch("data/player.csv").then(response => response.text()).then(text => {
     let parsed = Papa.parse(text, {
@@ -10,7 +54,7 @@ fetch("data/player.csv").then(response => response.text()).then(text => {
     });
 
     players = parsed;
-    console.log(parsed);
+    update();
 });
 
 fetch("data/rating.csv").then(response => response.text()).then(text => {
@@ -21,7 +65,7 @@ fetch("data/rating.csv").then(response => response.text()).then(text => {
     });
 
     ratings = parsed;
-    console.log(parsed);
+    update();
 });
 
 fetch("data/sets.csv").then(response => response.text()).then(text => {
@@ -32,7 +76,7 @@ fetch("data/sets.csv").then(response => response.text()).then(text => {
     });
 
     sets = parsed;
-    console.log(parsed);
+    update();
 });
 
 
