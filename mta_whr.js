96e99a29abaff47d1adee124b963a268c38bf3d4
Julien Vong <jvong@uwaterloo.ca>
2019-05-25 01:44:21 -0400

    More updates

--- mta_whr.js
+++ mta_whr.js
@@ -1,3 +1,9 @@
 fetch("data/player.csv").then(response => response.text()).then(text => {
-	console.log(text);
+    let csv = Papa.parse(text, {
+        header: true,
+        skipEmptyLines: true,
+        dynamicTyping: true,
+    });
+
+    console.log(csv);
 });
