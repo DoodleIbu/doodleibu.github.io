37422cfbb4376490d523e4877f36f0a8aa65c9bf
Julien Vong <jvong@uwaterloo.ca>
2019-05-25 02:03:02 -0400

    WIP

--- mta_whr.js
+++ mta_whr.js
@@ -1,9 +1,38 @@
+let players = []
+let ratings = []
+let sets = []
+
 fetch("data/player.csv").then(response => response.text()).then(text => {
+    let parsed = Papa.parse(text, {
+        header: true,
+        skipEmptyLines: true,
+        dynamicTyping: true,
+    });
+
+    players = parsed;
+    console.log(csv);
+});
+
+fetch("data/rating.csv").then(response => response.text()).then(text => {
+    let parsed = Papa.parse(text, {
+        header: true,
+        skipEmptyLines: true,
+        dynamicTyping: true,
+    });
+
+    ratings = parsed;
+    console.log(csv);
+});
+
+fetch("data/sets.csv").then(response => response.text()).then(text => {
     let csv = Papa.parse(text, {
         header: true,
         skipEmptyLines: true,
         dynamicTyping: true,
     });
 
+    sets = parsed;
     console.log(csv);
 });
+
+
